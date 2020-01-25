f5df65497d49c245296d6f0e24b003a657bd7890
Amalan <charizard0008@gmail.com>
2020-01-25 07:35:16 +0530

    Added Loading animation between sign in and register

--- lib/screens/authenticate/registerform.dart
+++ lib/screens/authenticate/registerform.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:letsmeet/services/auth.dart';
+import 'package:letsmeet/shared/constants.dart';
+import 'package:letsmeet/shared/loading.dart';
 
 class RegisterForm extends StatefulWidget {
   final Function signInState;
@@ -13,79 +15,91 @@ class _RegisterFormState extends State<RegisterForm> {
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
 
+  bool loading = false;
+
   String email = '';
   String password = '';
   String error = '';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: Text('Sign Up and Lets Eat'),
-        actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('Sign In'),
-            onPressed: () {
-              widget.signInState();
-            },
-          )
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 50, horizontal: 50),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              TextFormField(
-                validator: (val) => val.isEmpty ? 'Email is Empty' : null,
-                decoration: InputDecoration(hintText: 'Email'),
-                onChanged: (val) {
-                  setState(() {
-                    email = val;
-                  });
-                },
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown[100],
+            appBar: AppBar(
+              backgroundColor: Colors.brown[400],
+              elevation: 0.0,
+              title: Text('Sign Up and Lets Eat'),
+              actions: <Widget>[
+                FlatButton.icon(
+                  icon: Icon(Icons.person),
+                  label: Text('Sign In'),
+                  onPressed: () {
+                    widget.signInState();
+                  },
+                )
+              ],
+            ),
+            body: Container(
+              padding: EdgeInsets.symmetric(vertical: 50, horizontal: 50),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      validator: (val) => val.isEmpty ? 'Email is Empty' : null,
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'Email'),
+                      onChanged: (val) {
+                        setState(() {
+                          email = val;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      obscureText: true,
+                      validator: (val) => val.length < 6
+                          ? 'Password must more than 5 characters long'
+                          : null,
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'Password'),
+                      onChanged: (val) {
+                        setState(() {
+                          password = val;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    RaisedButton(
+                      color: Colors.green[40],
+                      child: Text('Register'),
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          setState(() {
+                            loading = true;
+                          });
+                          dynamic result = await _auth.RegisterWithEmailAndPass(
+                              email, password);
+                          if (result == null) {
+                            setState(() {
+                              error =
+                                  'Something went wrong, please try again later';
+                              loading = false;
+                            });
+                          }
+                        }
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    Text(error,
+                        style: TextStyle(color: Colors.red, fontSize: 14))
+                  ],
+                ),
               ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                obscureText: true,
-                validator: (val) => val.length < 6
-                    ? 'Password must more than 5 characters long'
-                    : null,
-                decoration: InputDecoration(hintText: 'Password'),
-                onChanged: (val) {
-                  setState(() {
-                    password = val;
-                  });
-                },
-              ),
-              SizedBox(height: 20.0),
-              RaisedButton(
-                color: Colors.green[40],
-                child: Text('Register'),
-                onPressed: () async {
-                  if (_formKey.currentState.validate()) {
-                    dynamic result =
-                        await _auth.RegisterWithEmailAndPass(email, password);
-                    if (result == null) {
-                      setState(() {
-                        error = 'Something went wrong, please try again later';
-                      });
-                    }
-                  }
-                },
-              ),
-              SizedBox(height: 20.0),
-              Text(error, style: TextStyle(color: Colors.red, fontSize: 14))
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:letsmeet/services/auth.dart';
+import 'package:letsmeet/shared/constants.dart';
+import 'package:letsmeet/shared/loading.dart';
 
 class SignIn extends StatefulWidget {
   final Function signInState;
@@ -14,6 +16,8 @@ class _SignInState extends State<SignIn> with SingleTickerProviderStateMixin {
 
   AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
+
+  bool loading = false;
 
   String email = '';
   String password = '';
@@ -33,71 +37,82 @@ class _SignInState extends State<SignIn> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: Text('Sing In to Eat Together'),
-        actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('Register'),
-            onPressed: () {
-              widget.signInState();
-            },
-          )
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 50, horizontal: 50),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              TextFormField(
-                validator: (val) => val.isEmpty ? 'Email is Empty' : null,
-                decoration: InputDecoration(hintText: 'Email'),
-                onChanged: (val) {
-                  setState(() {
-                    email = val;
-                  });
-                },
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown[100],
+            appBar: AppBar(
+              backgroundColor: Colors.brown[400],
+              elevation: 0.0,
+              title: Text('Sing In to Eat Together'),
+              actions: <Widget>[
+                FlatButton.icon(
+                  icon: Icon(Icons.person),
+                  label: Text('Register'),
+                  onPressed: () {
+                    widget.signInState();
+                  },
+                )
+              ],
+            ),
+            body: Container(
+              padding: EdgeInsets.symmetric(vertical: 50, horizontal: 50),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      validator: (val) => val.isEmpty ? 'Email is Empty' : null,
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'Email'),
+                      onChanged: (val) {
+                        setState(() {
+                          email = val;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      obscureText: true,
+                      validator: (val) =>
+                          val.length < 6 ? 'Password is Empty' : null,
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'Password'),
+                      onChanged: (val) {
+                        setState(() {
+                          password = val;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    RaisedButton(
+                      color: Colors.green[200],
+                      child: Text('Sign In'),
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          setState(() {
+                            loading = true;
+                          });
+                          dynamic result = await _auth.LoginWithEmailAndPass(
+                              email, password);
+                          if (result == null) {
+                            setState(() {
+                              error =
+                                  'Something went wrong, please try again later';
+                              loading = false;
+                            });
+                          }
+                        }
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    Text(error,
+                        style: TextStyle(color: Colors.red, fontSize: 14))
+                  ],
+                ),
               ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                obscureText: true,
-                validator: (val) => val.length < 6 ? 'Password is Empty' : null,
-                decoration: InputDecoration(hintText: 'Password'),
-                onChanged: (val) {
-                  setState(() {
-                    password = val;
-                  });
-                },
-              ),
-              SizedBox(height: 20.0),
-              RaisedButton(
-                color: Colors.green[200],
-                child: Text('Sign In'),
-                onPressed: () async {
-                  if (_formKey.currentState.validate()) {
-                    dynamic result =
-                        await _auth.LoginWithEmailAndPass(email, password);
-                    if (result == null) {
-                      setState(() {
-                        error = 'Something went wrong, please try again later';
-                      });
-                    }
-                  }
-                },
-              ),
-              SizedBox(height: 20.0),
-              Text(error, style: TextStyle(color: Colors.red, fontSize: 14))
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
